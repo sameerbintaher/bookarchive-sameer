63f78985cfeee320d3a26e4c0639619f8623d479
sameerbintaher <sameerbintaher@gmail.com>
2021-09-02 16:38:20 +0600

    okDone

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const searchBook = () => {
     }
     else {
         // load data
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data.docs))
